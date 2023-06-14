c18eb5726b1ed4d81b2c52cd206524ad5d346d8b
Andrii-Val <andrii.val1006@gmail.com>
2023-06-14 19:34:47 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+
+
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event){
+    
+    const{elements:{email, password}}=event.currentTarget;
+    if(email.value ==="" || password.value ===""){
+    alert("Заповніть будь-ласка пусті поля!")
+    } else{
+       const formData={
+            login:email.value,
+            password:password.value,
+        };
+        console.log(formData);
+    }
+
+event.currentTarget.reset();
+}
